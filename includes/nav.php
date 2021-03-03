94aba18b4b3bd538bd3470d74fef7ba4e5e3ed83
Rico <drraccoony@gmail.com>
2021-03-03 00:40:11 -0600

    Homepage and Nav changes

--- includes/nav.php
+++ includes/nav.php
@@ -1,25 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img src="img/brand.png" alt="" height="32" class="d-inline-block align-top">
-  501cVote</a>
+  <a class="navbar-brand" href="index.php"><img src="img/brand.png" alt="" height="32" class="d-inline-block align-top">
+  MNFurs Voting</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Candidates</a>
+        <a class="nav-link" href="https://www.mnfurs.org/forums/forum/mnfurs-forums/mnfurs-2021-elections-forum/meet-the-candidates/">Candidates</a>
       </li>
-      <li class="nav-item">
+      <!--<li class="nav-item">
         <a class="nav-link disabled" href="register.php">Register</a>
-      </li>
+      </li>-->
       <li class="nav-item">
         <a class="nav-link" href="validate_voter.php">Vote</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">About</a>
+        <a class="nav-link" href="about.php">About</a>
       </li>
     </ul>
   </div>
--- index.php
+++ index.php
@@ -7,8 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/501cVote.css">
     <?php include 'includes/head.php'; ?>
 
     <title>Hello, world!</title>
@@ -22,35 +20,13 @@
         <div class="py-5 text-center">
             <img class="d-block mx-auto mb-4" src="img/brand.png" alt="" width="72" height="72">
             <h2>MNFurs Voting</h2>
-            <p class="lead">Curabitur a felis in nunc fringilla tristique. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus.</p>
+            <p class="lead">Since we cannot vote in-person, its time to vote online!</p>
             <hr>
         </div>
         <p>Hello, MNFurs! This is your elections committee with an announcement to make. As you may or may not know, MNFurs is a volunteer-based 501c3 not-for-profit organization. What that means is that we not only have volunteers but also a board of directors to help things run smoothly within the group, and we will be starting the election process for two seats for board of directors.</p>
         <p>Starting on January 1, we will start accepting nominations for board members, so start thinking about if there is someone you believe would be a good fit for an MNFurs board position; otherwise you can nominate yourself as well.</p>
         <p>We will be using <a href="https://mnfurs.org">MNFurs website</a> to make full announcements throughout the whole process, but feel free to contact elections@mnfurs.org if you have any questions about things such as running for the board, how to vote in elections, or what is expected as a board member.</p>
     </div>
-
-    <div class="container">
-        <div class="card-deck mb-3 text-center">
-            <div class="card mb-4 box-shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Register</h4>
-                </div>
-                <div class="card-body">
-                    <p>Lorem Ipsum.</p>
-                    <button href="register.php" class="btn btn-lg btn-block btn-outline-primary">Submit Registration</button>
-                </div>
-            </div>
-            <div class="card mb-4 box-shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Vote</h4>
-                </div>
-                <div class="card-body">
-                    <p>Lorem Ipsum.</p>
-                    <button type="button" class="btn btn-lg btn-block btn-primary disabled">Get started</button>
-                </div>
-            </div>
-        </div>
 
 
         <?php include 'includes/footer.php'; ?>
