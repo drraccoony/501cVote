2de9af82b2cf09654d5f6fd11090e9b1f966c040
Rico <drraccoony@gmail.com>
2021-03-03 14:56:55 -0600

    Background Added

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <title>Hello, world!</title>
 </head>
 
-<body class="d-flex flex-column h-100">
+<body class="homebg d-flex flex-column h-100">
 
     <?php include 'includes/nav.php'; ?>
 
@@ -24,15 +24,13 @@
             <hr>
         </div>
         <p>Hello, MNFurs! This is your elections committee with an announcement to make. As you may or may not know, MNFurs is a volunteer-based 501c3 not-for-profit organization. What that means is that we not only have volunteers but also a board of directors to help things run smoothly within the group, and we will be starting the election process for two seats for board of directors.</p>
-        <p>Starting on January 1, we will start accepting nominations for board members, so start thinking about if there is someone you believe would be a good fit for an MNFurs board position; otherwise you can nominate yourself as well.</p>
-        <p>We will be using <a href="https://mnfurs.org">MNFurs website</a> to make full announcements throughout the whole process, but feel free to contact elections@mnfurs.org if you have any questions about things such as running for the board, how to vote in elections, or what is expected as a board member.</p>
+        <p>We will be using <a href="https://mnfurs.org">MNFurs website</a> to make full announcements throughout the whole process, but feel free to contact <code>elections@mnfurs.org</code> if you have any questions about things such as running for the board, how to vote in elections, or what is expected as a board member.</p>
     </div>
 
 
-        <?php include 'includes/footer.php'; ?>
-
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <?php include 'includes/footer.php'; ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
 
 </html>
