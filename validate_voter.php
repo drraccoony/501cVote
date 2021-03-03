4594980021af5c4b5b8d6f70e829238fc55bb81b
Rico <drraccoony@gmail.com>
2021-03-02 21:21:32 -0600

    Voter validation against DB

--- validate_voter.php
+++ validate_voter.php
@@ -15,7 +15,6 @@
 <body class="d-flex flex-column h-100">
 
     <?php include 'includes/nav.php'; ?>
-    <?php include 'dbconnect.php'; ?>
 
     <div class="container" role="main">
         <!--<h1>MNFurs - BoD Voting</h1>-->
@@ -26,18 +25,34 @@
                 <p class="lead">Upon internal confirmation with our internal system for volunteer hours, you should be provided a registration key for voting. When you cast your vote, your voting selections are completely unassociated from your identity.</p>
                 <p class="lead">Concerned about privacy? View our <a href="about.php">about page</a> to learn how we are handling voting this year.</p>
             </div>
+            <?php         
+        if (isset($_POST['submit'])) {
+            $result = checkExists();
+            if ($result) {
+                echo '<div class="alert alert-success" role="alert">
+                VoterID Found!</div>';
+                echo $_POST['voteid'];
+                header("Location:castvote.php?voteid=".$_POST['voteid']);
+            } else {
+                echo '<div class="alert alert-danger" role="alert">
+                Error. Your Voter code could not be found. Did you enter it correctly?</div>';
+            }
+        }?>
+            <h4>Testing Purposes</h4>
+            Valid: 646B7-957CS-NF7A3<br>
+            Not Valid: 646B7-957CS-AAAAA
 
             <div class="row">
                 <div class="col-md-12 order-md-1">
                     <h4 class="mb-3">Step 1: Validation</h4>
-                    <form class="" method="POST" action="validate_submit.php">
+                    <form class="" method="POST" action="">
                         <div class="mb-3">
                             <label for="username">Voting Key</label>
                             <div class="input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text"><i class="fas fa-key"></i></span>
                                 </div>
-                                <input type="text" class="form-control" id="voterkey" placeholder="M2GOI-H75AG-8S1I7" required>
+                                <input type="text" class="form-control" id="voterkey" name="voteid" placeholder="" required>
                                 <div class="invalid-feedback" style="width: 100%;">
                                     Your username is required.
                                 </div>
@@ -49,14 +64,58 @@
                             
                         <!--<hr class="mb-4">
                         <p class="lead">Upon clicking "Continue", you'll be given a voter code. This code will be used to cast your vote.</p>-->
-                        <button class="btn btn-primary btn-lg btn-block" type="submit">Continue</button>
+                        <button class="btn btn-primary btn-lg btn-block" type="submit" name="submit">Continue</button>
                     </form>
+
+                    <?php
+        function checkExists() {
+            include 'dbconnect.php';
+            $searchValue = trim($_POST['voteid']);
+            $sql = $conn->prepare("SELECT * FROM votes WHERE voterId = ?");
+            $sql->bind_param("s", $searchValue);
+            $sql->execute();
+            $result = $sql->get_result();
+            if ($result->num_rows > 0) {
+                return true;
+            } else {
+                return false;
+            }
+            $result->close();
+        }
+        ?>
+
+
+
                 </div>
             </div>
         </div>
 
     </div>
     <?php include 'includes/footer.php'; ?>
+
+
+
+    
+        <?php
+        /*
+        if (isset($_POST['submit'])) {
+            $searchValue = trim($_POST['voteid']);
+            include 'dbconnect.php';
+            if ($conn->connect_error) {
+                echo "connection Failed: " . $conn->connect_error;
+            } else {
+                $sql = "SELECT * FROM votes WHERE voterId LIKE '%$searchValue%'";
+
+                $result = $conn->query($sql);
+                while ($row = $result->fetch_assoc()) {
+                    echo $row['hours'] . "<br>";
+                }
+            }   
+        }
+        */
+        ?>
+
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
