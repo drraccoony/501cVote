9a4230be6f94c3d12a3d59fa30f04d7e0cda799e
Brian Moged <bmogged@zondahome.com>
2022-11-11 22:08:48 -0600

    Code reformmat
    Code reformatted by editor

--- castvote.php
+++ castvote.php
@@ -1,18 +1,18 @@
 <?php
 
-    // make it easier to convert over to db
+// make it easier to convert over to db
 $candidates = array(
-     (object) [
-    'name' =>    'Edward "Alli Coyote" Cardenas',
-    'id'=> 1
+    (object)[
+        'name' => 'Edward "Alli Coyote" Cardenas',
+        'id' => 1
     ],
     (object)[
-    'name'=>  'Caitlyn "Carmabella" Downen',
-    'id' => 2 ],
-    (object) ['name' =>'Jericho "Dirge" Nordstrom',
-    'id'=> 3],
-    (object) ['name'=> 'Douglas “Giza White Mage” Muth',
-    'id'=> 4]);
+        'name' => 'Caitlyn "Carmabella" Downen',
+        'id' => 2],
+    (object)['name' => 'Jericho "Dirge" Nordstrom',
+        'id' => 3],
+    (object)['name' => 'Douglas “Giza White Mage” Muth',
+        'id' => 4]);
 ?><!doctype html>
 <html lang="en">
 
@@ -40,93 +40,107 @@ $candidates = array(
 
 <body class="d-flex flex-column h-100">
 
-    <?php include 'includes/nav.php'; ?>
-    <?php $voter = $_GET['voteid']; ?>
-    <?php $debug = true; ?>
+<?php include 'includes/nav.php'; ?>
+<?php $voter = $_GET['voteid']; ?>
+<?php $debug = true; ?>
 
-    <div class="container" role="main">
-        <div class="container">
-            <div class="py-5 text-center">
-                <!-- steps -->
-                <ul id="progressbar" role="progressbar" aria-valuenow="Step 2 of 4 Vote" aria-valuetext="Step 2 of 4 Vote">
-                    <li class="active" id="step1"><strong>Validate</strong></li>
-                    <li class="active" id="step2" aria-valuetext="Step 2 of 4"><strong>Vote</strong></li>
-                    <li id="step3"><strong>Submit</strong></li>
-                    <li id="step4"><strong>Finished</strong></li>
-                </ul>
-                <!-- steps end -->
-                <i class="fas fa-tasks fa-6x" style="color: Dodgerblue;"></i>
-                <h1>Time to Vote!</h1>
-                <p class="lead">We found your voter ID in our system. Please make your voting selection for <strong>2</strong> candidates.</p>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12 order-md-1">
-                    <form class="" method="POST" action="">
-                        <div class="mb-3">
-                            <label for="voterkey">Voting Key</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="fas fa-key"></i></span>
-                                </div>
-                                <input type="text" class="form-control" id="voterkey" name="voteid" value="<?php echo (isset($voter)) ? $voter : ''; ?>" required disabled>
-                            </div>
-                        </div>
-                        <h2>Candidate Votes</h2>
-                        <p>Because we only have two seats available on the Board of Directors, Please select <strong>up to 2</strong> running candidates. Selecting more than 2 will result in your ballot being void, and not counted.</p>
-
-                        <!-- Start Checkboxes Loop through candidates -->
-                        <?php
-                        // Shuffle the candidates using internal ID to deal with the shuffling values
-                             shuffle($candidates);
-                            foreach ($candidates as &$candidate) {
-                                ?>
-                                <div class="form-check">
-                                    <input type="hidden" name="<?php echo 'candidate_'.$candidate->id ?>" value="0" />
-                                    <input class="form-check-input" type="checkbox" name="<?php echo 'candidate_'.$candidate->id ?>" id="<?php echo 'candidate_'.$candidate->id ?>" value="1">
-                                    <label class="form-check-label" for="<?php echo 'candidate_'.$candidate->id ?>">
-                                        <?php echo $candidate->name ?>
-                                    </label>
-                                </div>
-                                <?php
-                            }
-                        ?>
-                        <!-- End Checkboxes Loop through candidates -->
-
-                        <hr class="mb-4">
-                        <p class="lead">Once your vote is placed, you will not be able to alter your ballot!</p>
-                        <p>If you're not ready to vote, you may safely leave this page and come back again. However, once you vote, you cannot come back to change your vote.</p>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="accept" id="accept" required value="Yes">
-                            <label class="form-check-label" for="accept">
-                                I understand my vote cannot be changed once placed
-                            </label>
-                        </div><br>
-                        <button class="btn btn-success btn-lg btn-block" type="submit" name="submit"><i class="fas fa-check-square"></i> Place Vote!</button>
-                    </form>
-                </div>
-            </div>
+<div class="container" role="main">
+    <div class="container">
+        <div class="py-5 text-center">
+            <!-- steps -->
+            <ul id="progressbar" role="progressbar" aria-valuenow="Step 2 of 4 Vote" aria-valuetext="Step 2 of 4 Vote">
+                <li class="active" id="step1"><strong>Validate</strong></li>
+                <li class="active" id="step2" aria-valuetext="Step 2 of 4"><strong>Vote</strong></li>
+                <li id="step3"><strong>Submit</strong></li>
+                <li id="step4"><strong>Finished</strong></li>
+            </ul>
+            <!-- steps end -->
+            <i class="fas fa-tasks fa-6x" style="color: Dodgerblue;"></i>
+            <h1>Time to Vote!</h1>
+            <p class="lead">We found your voter ID in our system. Please make your voting selection for
+                <strong>2</strong> candidates.</p>
         </div>
 
+        <div class="row">
+            <div class="col-md-12 order-md-1">
+                <form class="" method="POST" action="">
+                    <div class="mb-3">
+                        <label for="voterkey">Voting Key</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="text" class="form-control" id="voterkey" name="voteid"
+                                   value="<?php echo (isset($voter)) ? $voter : ''; ?>" required disabled>
+                        </div>
+                    </div>
+                    <h2>Candidate Votes</h2>
+                    <p>Because we only have two seats available on the Board of Directors, Please select <strong>up to
+                            2</strong> running candidates. Selecting more than 2 will result in your ballot being void,
+                        and not counted.</p>
+
+                    <!-- Start Checkboxes Loop through candidates -->
+                    <?php
+                    // Shuffle the candidates using internal ID to deal with the shuffling values
+                    shuffle($candidates);
+                    foreach ($candidates as &$candidate) {
+                        ?>
+                        <div class="form-check">
+                            <input type="hidden" name="<?php echo 'candidate_' . $candidate->id ?>" value="0"/>
+                            <input class="form-check-input" type="checkbox"
+                                   name="<?php echo 'candidate_' . $candidate->id ?>"
+                                   id="<?php echo 'candidate_' . $candidate->id ?>" value="1">
+                            <label class="form-check-label" for="<?php echo 'candidate_' . $candidate->id ?>">
+                                <?php echo $candidate->name ?>
+                            </label>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <!-- End Checkboxes Loop through candidates -->
+
+                    <hr class="mb-4">
+                    <p class="lead">Once your vote is placed, you will not be able to alter your ballot!</p>
+                    <p>If you're not ready to vote, you may safely leave this page and come back again. However, once
+                        you vote, you cannot come back to change your vote.</p>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="accept" id="accept" required value="Yes">
+                        <label class="form-check-label" for="accept">
+                            I understand my vote cannot be changed once placed
+                        </label>
+                    </div>
+                    <br>
+                    <button class="btn btn-success btn-lg btn-block" type="submit" name="submit"><i
+                                class="fas fa-check-square"></i> Place Vote!
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
-    <?php include 'includes/footer.php'; ?>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+</div>
+<?php include 'includes/footer.php'; ?>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
 <?php
 /**
-*
-* 'Check' for a valid vote and save it
-*
-* @param $voterId - Voter key, $candidates array of candidates
-* @return array
-*  message - Success or error message
-*  success - if it was saved
-*
-*
-*/
+ *
+ * 'Check' for a valid vote and save it
+ *
+ * @param $voterId - Voter key, $candidates array of candidates
+ * @return array
+ *  message - Success or error message
+ *  success - if it was saved
+ *
+ *
+ */
 function checkSaveVote($voterId = '', $candidates = array())
 {
 /// default results if nothing happenns.
@@ -144,9 +158,9 @@ function checkSaveVote($voterId = '', $candidates = array())
         foreach ($candidates as &$canidate) {
             $candidateId = $canidate->id;
 
-                $sql .= ", `candidate{$candidateId}` = ?";
-                $fields .= 'i';
-                $parameters[] = intval($_POST['candidate_' . $candidateId]);
+            $sql .= ", `candidate{$candidateId}` = ?";
+            $fields .= 'i';
+            $parameters[] = intval($_POST['candidate_' . $candidateId]);
         }
 
         $sql .= ' WHERE `votes`.`voterId` = ? and voted = 0';
